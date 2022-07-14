e3c445812a217ebcd56385232d2ed9f16abc02fe
Robin Thöne <>
2022-07-14 20:46:03 +0200

    export types separated

---
+++ src/shared/types.ts
@@ -0,0 +1,6 @@
+import { IPokemon } from './types/IPokemon';
+import { IPokemonApiCache } from './types/IPokemonApiCache';
+import { IPokemonGameSave } from './types/IPokemonGameSave';
+import { IPokemonGeneration } from './types/IPokemonGeneration';
+
+export type { IPokemon, IPokemonApiCache, IPokemonGameSave, IPokemonGeneration };
