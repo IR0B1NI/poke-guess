9d715febdeb91d619de2a9333ed9cf276de94aae
Robin Thöne <>
2022-07-14 20:17:44 +0200

    add types to shared project

---
+++ src/shared/index.ts
@@ -0,0 +1,8 @@
+import { IPokemon } from './types/IPokemon';
+import { IPokemonApiCache } from './types/IPokemonApiCache';
+import { IPokemonGameSave } from './types/IPokemonGameSave';
+import { IPokemonGeneration } from './types/IPokemonGeneration';
+import { AlertType } from './types/AlertType';
+
+export type { IPokemon, IPokemonApiCache, IPokemonGameSave, IPokemonGeneration };
+export { AlertType };
