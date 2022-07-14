c6898604e2f88c135675bf1c5a5923678e5f9378
Robin Thöne <>
2022-07-14 21:26:06 +0200

    add enumerations to own folder

--- src/shared/src/index.ts
+++ src/shared/src/index.ts
@@ -1,3 +1,9 @@
-import { AlertType } from './types/AlertType';
+import { AlertType } from './enumerations/AlertType';
+import { IPokemon } from './types/IPokemon';
+import { IPokemonApiCache } from './types/IPokemonApiCache';
+import { IPokemonGameSave } from './types/IPokemonGameSave';
+import { IPokemonGeneration } from './types/IPokemonGeneration';
+
+export type { IPokemon, IPokemonApiCache, IPokemonGameSave, IPokemonGeneration };
 
 export { AlertType };
--- src/shared/types.ts
+++
@@ -1,6 +0,0 @@
-import { IPokemon } from './src/types/IPokemon';
-import { IPokemonApiCache } from './src/types/IPokemonApiCache';
-import { IPokemonGameSave } from './src/types/IPokemonGameSave';
-import { IPokemonGeneration } from './src/types/IPokemonGeneration';
-
-export type { IPokemon, IPokemonApiCache, IPokemonGameSave, IPokemonGeneration };
