f9acffabbf474c48f753acc7d307d0dd6d898c2e
Robin Thöne <>
2022-07-14 21:03:00 +0200

    move code to src folder

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,6 +1,6 @@
-import { IPokemon } from './types/IPokemon';
-import { IPokemonApiCache } from './types/IPokemonApiCache';
-import { IPokemonGameSave } from './types/IPokemonGameSave';
-import { IPokemonGeneration } from './types/IPokemonGeneration';
+import { IPokemon } from './src/types/IPokemon';
+import { IPokemonApiCache } from './src/types/IPokemonApiCache';
+import { IPokemonGameSave } from './src/types/IPokemonGameSave';
+import { IPokemonGeneration } from './src/types/IPokemonGeneration';
 
 export type { IPokemon, IPokemonApiCache, IPokemonGameSave, IPokemonGeneration };
