976abbb3063a3b03bde8b067cc17858ecc17585a
Robin Thöne <>
2022-07-20 22:43:52 +0200

    add game save to the application store

--- src/mobile/store/models/applicationModel.ts
+++ src/mobile/store/models/applicationModel.ts
@@ -1,4 +1,6 @@
-import { Action, action } from 'easy-peasy';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Action, action, Thunk, thunk } from 'easy-peasy';
+import { IPokemonGameSave, saveGameState } from 'poke-guess-shared';
 
 /**
  * Interface for the application model.
@@ -8,6 +10,12 @@ export interface IApplicationModel {
     isBottomNavBarHidden: boolean;
     /** Action to update Whether the bottom nav bar is hidden or not. */
     updateIsBottomNavBarHidden: Action<IApplicationModel, boolean>;
+    /** The game save model. */
+    gameSave?: IPokemonGameSave;
+    /** Action to update the game save model. */
+    updateGameSave: Action<IApplicationModel, IPokemonGameSave>;
+    /** Action to update the game save model asynchronous. */
+    updateGameSaveAsync: Thunk<IApplicationModel, IPokemonGameSave>;
 }
 
 /**
@@ -17,5 +25,13 @@ export const ApplicationModel: IApplicationModel = {
     isBottomNavBarHidden: false,
     updateIsBottomNavBarHidden: action((state, payload) => {
         state.isBottomNavBarHidden = payload;
+    }),
+    gameSave: undefined,
+    updateGameSave: action((state, payload) => {
+        state.gameSave = payload;
+    }),
+    updateGameSaveAsync: thunk(async (actions, payload) => {
+        actions.updateGameSaveAsync(payload);
+        await saveGameState(payload.generationNames, payload.foundPokemonNames, AsyncStorage.setItem);
     }),
 };
