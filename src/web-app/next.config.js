aad38d211cf0c2cbf71d5a501b96a660a65e2cb5
Robin Thöne <>
2022-07-21 19:39:01 +0200

    fix nextjs build

--- src/web-app/next.config.js
+++ src/web-app/next.config.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require('./next-i18next.config');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')(['poke-guess-shared']);
 
-module.exports = {
+module.exports = withTM({
     i18n,
     images: {
         domains: ['raw.githubusercontent.com'],
     },
-};
+});
